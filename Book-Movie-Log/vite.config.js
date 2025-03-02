69fa71038fa905f5acaa529ac60f3a2153111513
Sania Pervez <139937275+sanipervez@users.noreply.github.com>
2025-03-02 13:16:31 -0500

    Fix base path for GitHub Pages

--- Book-Movie-Log/vite.config.js
+++ Book-Movie-Log/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BookLog/', // This should match your repo name
+  base: '/BookLog/', 
+  build: {
+    outDir: 'docs', 
+  }
 });
